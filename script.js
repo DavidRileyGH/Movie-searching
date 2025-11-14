89f5fca4f47084ccd6725abee45a1bb5834d94b3
DavidRileyGH <being.still@hotmail.com>
2025-11-14 15:35:15 -0600

    fixed html

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-const API_KEY = "http://www.omdbapi.com/?apikey=[3e577df2]&";
+const API_KEY = "http://www.omdbapi.com/?apikey=3e577df2&";
 
 function handleSearch() {
-  const query = document.getElementById("search").value;
-  searchMovies(query);
+    const query = document.getElementById("search").value;
+    searchMovies(query);
 }
 
 function searchMovies(query) {
-  fetch(`https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${query}`)
-    .then(res => res.json())
-    .then(data => showMovies(data.results))
-    .catch(err => console.error(err));
+    fetch (https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${query})
+        .then(res => res.json())
+        .then(data => showMovies(data.results))
+        .catch(err => console.error(err));
 }
 
 function showMovies(movies) {
-  const container = document.getElementById("results");
-  container.innerHTML = movies.map(m => `<p>${m.title}</p>`).join("");
+    const container = document.getElementById("results");
+    container.innerHTML = movies.map(m => <p>${m.title}</p>).join("");
 }
